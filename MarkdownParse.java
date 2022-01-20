6c655ff0cb1f094a0132e9ec4dd5ca332a362b79
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2022-01-19 17:20:40 -0800

    Update MarkdownParse.java
    Improved fix that works even when parenthesis are present as part of text

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,7 +10,7 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        if (!markdown.contains("(") && !markdown.contains("[")) {
+        if (!markdown.contains("(") || !markdown.contains("[")) {
             return toReturn;
         }
 
