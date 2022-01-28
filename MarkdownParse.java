cd8b33c26dca974432f27274b87deed1ae69d1f7
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2022-01-27 21:30:20 -0800

    Fix for error where a link with parentheses is not printed correctly
    Original Error:
    
    PS C:\Users\hnvul\Documents\GitHub\markdown-parse> java MarkdownParse linkParentheses.md
    [niceLink(, anotherNiceLink.com]

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,28 +9,36 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int imageIndex = markdown.indexOf("!", currentIndex);
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
+        String[] linkArray = markdown.split("\n");
+        for(String link: linkArray) {
+            int currentIndex = 0;
+            while(currentIndex < link.length()) {
+                int imageIndex = link.indexOf("!", currentIndex);
+                int nextOpenBracket = link.indexOf("[", currentIndex);
+                int nextCloseBracket = link.indexOf("]", nextOpenBracket);
+                int openParen = link.indexOf("(", nextCloseBracket);
+                int closeParen = link.lastIndexOf(")");
 
-            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
-                break;
-            }
+                if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
+                    break;
+                }
 
-            if (imageIndex != -1 && imageIndex == nextOpenBracket - 1) {
-                currentIndex = closeParen + 1;
-            } else {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
+                if (imageIndex != -1 && imageIndex == nextOpenBracket - 1) {
+                    currentIndex = closeParen + 1;
+                } else {
+                    toReturn.add(link.substring(openParen + 1, closeParen));
+                    currentIndex = closeParen + 1;
+                }
             }
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
+        if(args.length == 0) {
+            System.out.println("No file name given");
+            return;
+        }
+        
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
