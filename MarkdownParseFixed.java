2031daa9ed1ea18db29f12e20627960427e0edc2
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2022-01-27 20:40:27 -0800

    Temporarily reverting changes
    Reverting MarkdownParse.java back to the original, and temporarily storing my complete changes in the file MarkdownParseFixed.java

--- MarkdownParseFixed.java
+++ MarkdownParseFixed.java
@@ -4,23 +4,38 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-public class MarkdownParse {
+public class MarkdownParseFixed {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
+        if (!markdown.contains("(") && !markdown.contains(")") && !markdown.contains("[") && !markdown.contains("]")) {
+            return toReturn;
+        }
+
         while(currentIndex < markdown.length()) {
+            int imageIndex = markdown.indexOf("!", currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+
+            if (imageIndex != -1 && imageIndex < nextOpenBracket) {
+                currentIndex = closeParen + 1;
+            } else {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
+        if(args.length == 0) {
+            System.out.println("No file name given");
+            return;
+        }
+        
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
