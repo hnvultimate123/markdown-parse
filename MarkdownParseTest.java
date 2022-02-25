60176cf2e67a79d4c9ce3f16dcbe57672c8cb9a0
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2022-02-24 18:22:55 -0800

    New tests + snippets for lab report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,18 +4,26 @@ import org.junit.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MarkdownParseTest {
+    
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void snippetTest1() throws IOException {
+        assertEquals(List.of("url.com", "`google.com", "google.com"), MarkdownParse.getLinks(Files.readString(Path.of("snippet1.md"))));
     }
 
     @Test
-    public void getLinksTest() throws IOException {
-        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
-        assertEquals(List.of("link.com", "link2.com"), MarkdownParse.getLinks(Files.readString(Path.of("linksInLine.md"))));
+    public void snippetTest2() throws IOException {
+        assertEquals(List.of("b.com", "a.com(())"), MarkdownParse.getLinks(Files.readString(Path.of("snippet2.md"))));
+    }
+
+    @Test
+    public void snippetTest3() throws IOException {
+        ArrayList<String> emptyList = new ArrayList<>();
+        assertEquals(emptyList, MarkdownParse.getLinks(Files.readString(Path.of("snippet3.md"))));
+
     }
 
     
